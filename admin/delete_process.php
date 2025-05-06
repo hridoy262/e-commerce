bb8ad808f33938275a0af9beb6159d63b693327c
Hridoy Ahmed <85163279+hridoy262@users.noreply.github.com>
2025-05-07 00:27:29 +0600

    Add files via upload

---
+++ admin/delete_process.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'lib/connection.php';
+
+"DELETE FROM users WHERE `users`.`id` = 1"
+
+    if(mysqli_query($conn,$sql))
+
+
+
+
+
+// $sql = "DELETE FROM users WHERE $row["id"];
+// if (mysqli_query($conn, $sql)) {
+//     echo "Record deleted successfully";
+// } else {
+//     echo "Error deleting record: " . mysqli_error($conn);
+// }
+// mysqli_close($conn);
+// ?>
---
+++ footer.php
@@ -0,0 +1,97 @@
+
+
+
+
+<br>
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 text-center">
+        <img src="img/logo.png" class="footer-img">
+        <p>বাংলাদেশের সবচেয়ে জনপ্রিয় অনলাইন শপিং ওয়েবসাইট, যেখানে আকর্ষণীয় বিভিন্ন ডিল এবং ডিসকাউন্ট অফার সহ ফ্যাশন সামগ্রী, মোবাইল ফোন, ইলেকট্রনিক এক্সেসরিজ এবং গৃহস্থালিসংক্রান্ত পণ্য সমূহ এখন একদম সাশ্রয়ী মূল্যে পাচ্ছেন। 
+       
+        <img src="img/social.png">
+        
+      </div>
+      <span id="footer-line"></span>
+      <div class="col-lg-3 text-center">
+        
+        <h5 class="footer-header">my account</h5>
+        <p>
+    Pikolo সম্পর্কে  <br>
+    ডিজিটাল পেমেন্টস <br>
+    pikolo Card<br>
+    pikolo ব্লগ<br>
+    pikolo কেয়ার<br>
+    pikolo মার্ট<br>
+    সুরক্ষা নীতিমালা<br>
+
+      </div>
+       <span  id="footer-line"></span>
+      <div class="col-lg-4 text-center">
+
+       <h5 class="footer-header">Shop</h5>
+
+      <p>Connect With Pikolos<br>
+      Earn With Pikolo<br>
+      Pikolos University<br>
+      Code of Conduct<br>
+      Join the Pikolos Affiliate Program</p>
+        
+        
+      </div>
+
+      
+    </div>
+  </div>
+</div>
+
+
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <span id="last-footer-line"></span>
+    </div>
+    <div class="col-md-12 text-center">
+      <img src="img/footerful.png" class="last-pic">
+    </div>
+  </div>
+</div>
+<br>
+
+
+<div class="footer1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p>Site Map  Search Terms  Advanced Search Orders and Returns <b> Contact Us </b></p>
+        <p>© 2023 CodeError. All Rights Reserved <b style="color:orange;"> Code Erorr </b>.</p>
+        <img src="img/pay.png" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+  
+
+ 
+
+
+
+  <script src="https://kit.fontawesome.com/3b83a3096d.js" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <script src="js/isotope.js"></script>
+  <script src="js/isotope-active.js"></script>
+
+</body>
+</html>
